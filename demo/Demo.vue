5af9026566860c79f3380a9d1678ec1d717ee67f
gaoshuai0525 <gaoshuai0525@gmail.com>
2018-01-30 14:45:27 +0800

    vue-tree 简单好用的树形组件

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,73 +1,82 @@
 <template>
-        <v-tree id="app" :options="treeOptions">
-            <div slot="renderContent" slot-scope="props" class="s_tr">
+    <v-tree id="app" :options="treeOptions">
+        <div slot="renderContent" slot-scope="props" class="s_tr">
 
-                <div class="s_content">
-                    <li>
-                        {{props.item?props.item.name:null}}
-                    </li>
-                    <li>{{props.item?props.item.des:null}}</li>
-                    <li class="s_t_option">
-                        <button class="add" @click="addClick(props.item)">添加</button>
-                        <button class="delete" @click="deleteClick(props.item,props.options)">删除</button>
-                    </li>
-                </div>
+            <div class="s_content">
+                <li>
+                    {{props.item?props.item.name:null}}
+                </li>
+                <li>{{props.item?props.item.des:null}}</li>
+                <li class="s_t_option">
+                    <button class="add" @click="addClick(props.item)">添加</button>
+                    <button class="delete" @click="deleteClick(props.item,props.options)">删除</button>
+                </li>
             </div>
-        </v-tree>
+        </div>
+    </v-tree>
 </template>
 
 <script>
 
-import vTree from '../src/index'
+    import vTree from '../src/index'
 
-export default {
-  components: {
-    'v-tree': vTree
-  },
-  data () {
-    return {
-        treeOptions: [
-            {name:'1',des:'1',code:'1',
-                child:[
-                    {name:'1-1',des:'1-1',code:'1-1'}]},
-            {name:'2',des:'2',code:'2',child:[
-                {name:'2-1',des:'2-1', code:'2-1', child:[
-                    {name:'2-1-1',des:'2-1-1',code:'2-1-1'},
-                    {name:'2-1-2',des:'2-1-2',code:'2-1-2',child:[
-                        {name:'2-1-2-1',des:'2-1-2-1',code:'2-1-2-1'}
-                    ]}
-                ]}
-            ]}
-        ],
+    export default {
+        components: {
+            'v-tree': vTree
+        },
+        data () {
+            return {
+                treeOptions: [
+                    {
+                        name: '1', des: '1', code: '1',
+                        child: [
+                            {name: '1-1', des: '1-1', code: '1-1'}]
+                    },
+                    {
+                        name: '2', des: '2', code: '2', child: [
+                        {
+                            name: '2-1', des: '2-1', code: '2-1', child: [
+                            {name: '2-1-1', des: '2-1-1', code: '2-1-1'},
+                            {
+                                name: '2-1-2', des: '2-1-2', code: '2-1-2', child: [
+                                {name: '2-1-2-1', des: '2-1-2-1', code: '2-1-2-1'}
+                            ]
+                            }
+                        ]
+                        }
+                    ]
+                    }
+                ],
+            }
+        },
+
+        methods: {
+            deleteClick(item, options){
+                var index = options.findIndex(obj => item == obj)
+                options.splice(index, 1)
+            },
+            addClick(item){
+                if (!item.child) {
+                    this.$set(item, 'child', [])
+                }
+                item.child.push({
+                    name: item.name + '-' + (item.child.length + 1),
+                    des: item.name + '-' + (item.child.length + 1),
+                    code: item.name + '-' + (item.child.length + 1)
+                })
+
+            }
+        },
+
     }
-  },
-
-  methods: {
-      deleteClick(item,options){
-          var index = options.findIndex(obj => item == obj)
-          options.splice(index,1)
-      },
-      addClick(item){
-          if(!item.child){
-              this.$set(item,'child',[])
-          }
-              item.child.push({
-                  name:item.name+'-'+(item.child.length+1),
-                  des:item.name+'-'+(item.child.length+1),
-                  code:item.name+'-'+(item.child.length+1)
-              })
-
-      }
-  },
-
-}
 </script>
 
 <style lang="stylus">
-    .delete{
+    .delete {
         margin-left: 4rem;
     }
-    .add, .delete{
+
+    .add, .delete {
         display: inline-block;
         line-height: .1;
         white-space: nowrap;
@@ -88,32 +97,36 @@ export default {
         padding: 12px 20px;
         border-radius: 4px;
     }
-    #app{
-        width:97%;
-        background:#e0eef2;
-        padding:1rem 0.7rem 1rem 1rem;
+
+    #app {
+        width: 97%;
+        background: #e0eef2;
+        padding: 1rem 0.7rem 1rem 1rem;
         box-sizing: border-box;
-        margin:0 auto;
-        margin-top:-0.8rem;
+        margin: 0 auto;
+        margin-top: -0.8rem;
     }
-    .s_tr{
+
+    .s_tr {
         border-collapse: collapse;
-        background:#e0eef2;
-        box-shadow:none;
-        margin:0;
+        background: #e0eef2;
+        box-shadow: none;
+        margin: 0;
         margin-left: 30px;
         overflow: hidden;
     }
-    .s_tr li{
+
+    .s_tr li {
         width: 25%;
         display: table-cell;
         text-align: center;
     }
-    .s_tr .s_content{
+
+    .s_tr .s_content {
         display: table;
-        background:#fff;
+        background: #fff;
         width: 100%;
         margin-bottom: 10px;
-        box-shadow:2px 3px 5px rgba(167, 142, 142, 0.1);
+        box-shadow: 2px 3px 5px rgba(167, 142, 142, 0.1);
     }
 </style>
--- src/components/tree.vue
+++ src/components/tree.vue
@@ -1,20 +1,20 @@
 <template>
     <transition name='fade'>
-    <ul class="tree" v-show="visible">
-        <li class="treenode" v-for="(item,i) in options">
-            <img src="./up_two.png" @click="iconClick($event)" :class="nodes[i].extend ? 'expand' :''"/>
-            <slot name="renderContent" :item="item" :options="options" >
-            </slot>
-            <template v-if="item.child">
-                <vueTree :options="item.child">
-                    <img src="./up_two.png" @click="iconClick($event)"/>
-                    <template class="node-content" slot="renderContent" slot-scope="props">
-                        <slot name="renderContent" :item="props.item" :options="props.options"></slot>
-                    </template>
-                </vueTree>
-            </template>
-        </li>
-    </ul>
+        <ul class="tree" v-show="visible">
+            <li class="treenode" v-for="(item,i) in options">
+                <img src="./up_two.png" @click="iconClick($event)" :class="nodes[i].extend ? 'expand' :''"/>
+                <slot name="renderContent" :item="item" :options="options">
+                </slot>
+                <template v-if="item.child">
+                    <vueTree :options="item.child">
+                        <img src="./up_two.png" @click="iconClick($event)"/>
+                        <template class="node-content" slot="renderContent" slot-scope="props">
+                            <slot name="renderContent" :item="props.item" :options="props.options"></slot>
+                        </template>
+                    </vueTree>
+                </template>
+            </li>
+        </ul>
     </transition>
 </template>
 
@@ -24,8 +24,7 @@
         props: {
             options: Array
         },
-        components:{
-        },
+        components: {},
         data () {
             return {
                 visible: true,
@@ -46,50 +45,57 @@
         methods: {
             iconClick(event){
                 var children = this.$children;
-                for(var i = 0;i < children.length; i++){
-                    if(children[i].$el.parentElement.contains(event.target)){
-                        children[i].visible = children[i].visible ? false: true;
+                for (var i = 0; i < children.length; i++) {
+                    if (children[i].$el.parentElement.contains(event.target)) {
+                        children[i].visible = children[i].visible ? false : true;
                         this.nodes[i].extend = children[i].visible;
                     }
                 }
             },
             extendNodeValue(options){
-                for(var i = 0; i < options.length; i++){
-                    this.nodes.push({data: options[i],extend: true})
+                for (var i = 0; i < options.length; i++) {
+                    this.nodes.push({data: options[i], extend: true})
                 }
             }
         }
     }
 </script>
 <style scoped>
-    ul,li{
+    ul, li {
         list-style: none;
         margin: 0;
         padding: 0;
     }
-    .tree{
+
+    .tree {
         overflow: hidden;
     }
-    .treenode{
+
+    .treenode {
         padding-left: 30px;
         line-height: 36px;
     }
-    .node-content{
+
+    .node-content {
         display: inline-block;
     }
-    img{
+
+    img {
         float: left;
         padding: 8px;
         margin-right: 8px;
     }
-    .fade-enter-active,.fade-leave-active{
-        transition:height 5s ease;
+
+    .fade-enter-active, .fade-leave-active {
+        transition: height 5s ease;
     }
-    .fade-enter,.fade-leave-active{
+
+    .fade-enter, .fade-leave-active {
         height: 0;
         padding-bottom: 0;
     }
-    .expand{
+
+    .expand {
         transform: rotate(90deg);
     }
 </style>
